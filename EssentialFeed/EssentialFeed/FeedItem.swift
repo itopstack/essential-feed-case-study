990bc245a380ed9a3f02d16c73963469e271e30f
Kittisak Phetrungnapha <cs.sealsoul@gmail.com>
2022-04-06 16:48:35 +0700

    Create FeedItem model

---
+++ EssentialFeed/EssentialFeed/FeedItem.swift
@@ -0,0 +1,15 @@
+//
+//  FeedItem.swift
+//  EssentialFeed
+//
+//  Created by Kittisak Phetrungnapha on 6/4/2565 BE.
+//
+
+import Foundation
+
+struct FeedItem {
+    let id: UUID
+    let description: String?
+    let location: String?
+    let imageURL: URL
+}
