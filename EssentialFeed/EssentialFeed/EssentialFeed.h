2b4f35669c70f1fc14029dd607b042b6652f3773
Kittisak Phetrungnapha <cs.sealsoul@gmail.com>
2022-04-06 16:47:17 +0700

    Add initial files

---
+++ EssentialFeed/EssentialFeed/EssentialFeed.h
@@ -0,0 +1,18 @@
+//
+//  EssentialFeed.h
+//  EssentialFeed
+//
+//  Created by Kittisak Phetrungnapha on 6/4/2565 BE.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for EssentialFeed.
+FOUNDATION_EXPORT double EssentialFeedVersionNumber;
+
+//! Project version string for EssentialFeed.
+FOUNDATION_EXPORT const unsigned char EssentialFeedVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <EssentialFeed/PublicHeader.h>
+
+
