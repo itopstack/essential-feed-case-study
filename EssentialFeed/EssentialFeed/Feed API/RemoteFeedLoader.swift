f2bff1ff8ad0025d8301996d8fcef72895c04c49
Kittisak Phetrungnapha <cs.sealsoul@gmail.com>
2022-04-08 19:13:11 +0700

    Refactor hard coded to be descriptive variable and function

--- EssentialFeed/EssentialFeed/Feed API/RemoteFeedLoader.swift
+++ EssentialFeed/EssentialFeed/Feed API/RemoteFeedLoader.swift
@@ -63,8 +63,12 @@ private class FeedItemsMapper {
         }
     }
     
+    private static var OK_200: Int {
+        200
+    }
+    
     static func map(_ data: Data, response: HTTPURLResponse) throws -> [FeedItem] {
-        guard response.statusCode == 200 else {
+        guard response.statusCode == OK_200 else {
             throw RemoteFeedLoader.Error.invalidData
         }
         let root = try JSONDecoder().decode(Root.self, from: data)
