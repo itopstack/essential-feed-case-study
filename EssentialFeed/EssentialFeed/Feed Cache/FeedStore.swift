c6e57edd9cc4a0079dcf827691d73b184cb24d05
Kittisak Phetrungnapha <cs.sealsoul@gmail.com>
2022-04-16 17:32:05 +0700

    Move FeedStore to its own file

---
+++ EssentialFeed/EssentialFeed/Feed Cache/FeedStore.swift
@@ -0,0 +1,16 @@
+//
+//  FeedStore.swift
+//  EssentialFeed
+//
+//  Created by Kittisak Phetrungnapha on 16/4/2565 BE.
+//
+
+import Foundation
+
+public protocol FeedStore {
+    typealias DeletionCompletion = (Error?) -> Void
+    typealias InsertionCompletion = (Error?) -> Void
+    
+    func deleteCachedFeed(completion: @escaping DeletionCompletion)
+    func insert(_ items: [FeedItem], timestamp: Date, completion: @escaping (InsertionCompletion))
+}
