fc6fa9bc68ceb7e6e046924c26b1bdaba55f8232
Kittisak Phetrungnapha <cs.sealsoul@gmail.com>
2022-04-25 13:50:27 +0700

    Dispatch `CodableFeedStore` operations in a serial background queue to avoid blocking clients

--- EssentialFeed/EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed Cache/CodableFeedStoreTests.swift
@@ -131,26 +131,26 @@ final class CodableFeedStoreTests: XCTestCase {
     func test_storeSideEffects_runSerially() {
         let sut = makeSUT()
         var completedOperationInOrder: [XCTestExpectation] = []
-        
+
         let op1 = expectation(description: "Operation 1")
         sut.insert(uniqueFeedImages().locals, timestamp: Date()) { _ in
             completedOperationInOrder.append(op1)
             op1.fulfill()
         }
-        
+
         let op2 = expectation(description: "Operation 2")
         sut.deleteCachedFeed { _ in
             completedOperationInOrder.append(op2)
             op2.fulfill()
         }
-        
+
         let op3 = expectation(description: "Operation 3")
         sut.insert(uniqueFeedImages().locals, timestamp: Date()) { _ in
             completedOperationInOrder.append(op3)
             op3.fulfill()
         }
         
-        wait(for: completedOperationInOrder, timeout: 5.0)
+        waitForExpectations(timeout: 5)
         
         XCTAssertEqual(completedOperationInOrder, [op1, op2, op3], "Expected side-effects to run serially but operations finished in the wrong order")
     }
@@ -210,7 +210,7 @@ final class CodableFeedStoreTests: XCTestCase {
             deletionError = receivedDeletionError
             exp.fulfill()
         }
-        wait(for: [exp], timeout: 1)
+        wait(for: [exp], timeout: 3)
         return deletionError
     }
     
