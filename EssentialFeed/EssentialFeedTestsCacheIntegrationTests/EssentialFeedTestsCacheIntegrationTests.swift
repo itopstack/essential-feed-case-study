75b5412a9c269afc61b0b28bb708623fb5251196
Kittisak Phetrungnapha <cs.sealsoul@gmail.com>
2022-04-28 18:44:49 +0700

    Add `EssentialFeedCacheIntegrationTests` target

---
+++ EssentialFeed/EssentialFeedTestsCacheIntegrationTests/EssentialFeedTestsCacheIntegrationTests.swift
@@ -0,0 +1,14 @@
+//
+//  EssentialFeedTestsCacheIntegrationTests.swift
+//  EssentialFeedTestsCacheIntegrationTests
+//
+//  Created by Kittisak Phetrungnapha on 28/4/2565 BE.
+//
+
+import XCTest
+import EssentialFeed
+
+class EssentialFeedTestsCacheIntegrationTests: XCTestCase {
+
+    
+}
