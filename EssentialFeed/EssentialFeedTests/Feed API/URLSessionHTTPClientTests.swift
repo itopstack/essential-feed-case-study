2906f6139839c711435bbf8e4b1f6d17cd2683bc
Kittisak Phetrungnapha <cs.sealsoul@gmail.com>
2022-05-02 14:23:35 +0700

    Nest `HTTPClientResult` inside the `HTTPClient` protocol since they are closely related

--- EssentialFeed/EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
+++ EssentialFeed/EssentialFeedTests/Feed API/URLSessionHTTPClientTests.swift
@@ -104,12 +104,12 @@ final class URLSessionHTTPClientTests: XCTestCase {
         }
     }
     
-    private func resultFor(data: Data?, response: URLResponse?, error: Error?, file: StaticString = #filePath, line: UInt = #line) -> HTTPClientResult {
+    private func resultFor(data: Data?, response: URLResponse?, error: Error?, file: StaticString = #filePath, line: UInt = #line) -> HTTPClient.Result {
         URLProtocolStub.stub(data: data, response: response, error: error)
         let sut = makeSUT(file: file, line: line)
         let exp = expectation(description: "Wait for completion")
         
-        var capturedResult: HTTPClientResult!
+        var capturedResult: HTTPClient.Result!
         sut.get(from: anyURL()) { result in
             capturedResult = result
             exp.fulfill()
