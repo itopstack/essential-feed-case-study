ca0f8ba5a1198fa69497a140b2a12b0e211685f0
Kittisak Phetrungnapha <cs.sealsoul@gmail.com>
2022-05-09 22:51:55 +0700

    Render loaded feed on successful load completion

---
+++ EssentialFeed/EsstentialFeediOS/FeedImageCell.swift
@@ -0,0 +1,15 @@
+//
+//  FeedImageCell.swift
+//  EsstentialFeediOS
+//
+//  Created by Kittisak Phetrungnapha on 9/5/2565 BE.
+//
+
+import Foundation
+import UIKit
+
+public final class FeedImageCell: UITableViewCell {
+    public let locationContainer = UIView()
+    public let locationLabel = UILabel()
+    public let descriptionLabel = UILabel()
+}
