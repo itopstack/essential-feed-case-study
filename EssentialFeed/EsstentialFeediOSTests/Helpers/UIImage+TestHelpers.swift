d6a65a6a9cfa03b7261436706398b1639e21c564
Kittisak Phetrungnapha <cs.sealsoul@gmail.com>
2022-05-16 21:19:29 +0700

    Extract UIImage test helper to a separate file

---
+++ EssentialFeed/EsstentialFeediOSTests/Helpers/UIImage+TestHelpers.swift
@@ -0,0 +1,21 @@
+//
+//  UIImage+TestHelpers.swift
+//  EsstentialFeediOSTests
+//
+//  Created by Kittisak Phetrungnapha on 16/5/2565 BE.
+//
+
+import UIKit
+
+extension UIImage {
+    static func make(withColor color: UIColor) -> UIImage {
+        let rect = CGRect(x: 0, y: 0, width: 1, height: 1)
+        UIGraphicsBeginImageContext(rect.size)
+        let context = UIGraphicsGetCurrentContext()!
+        context.setFillColor(color.cgColor)
+        context.fill(rect)
+        let img = UIGraphicsGetImageFromCurrentImageContext()
+        UIGraphicsEndImageContext()
+        return img!
+    }
+}
