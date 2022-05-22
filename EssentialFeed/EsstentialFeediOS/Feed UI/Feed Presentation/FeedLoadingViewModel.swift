23156d5246f5669dca25dfd0e31ebced8074d658
Kittisak Phetrungnapha <cs.sealsoul@gmail.com>
2022-05-22 18:59:42 +0700

    Move ViewModels to separate files

---
+++ EssentialFeed/EsstentialFeediOS/Feed UI/Feed Presentation/FeedLoadingViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  FeedLoadingViewModel.swift
+//  EsstentialFeediOS
+//
+//  Created by Kittisak Phetrungnapha on 22/5/2565 BE.
+//
+
+import Foundation
+
+struct FeedLoadingViewModel {
+    let isLoading: Bool
+}
---
+++ EssentialFeed/EsstentialFeediOS/Feed UI/Feed Presentation/FeedViewModel.swift
@@ -0,0 +1,12 @@
+//
+//  FeedViewModel.swift
+//  EsstentialFeediOS
+//
+//  Created by Kittisak Phetrungnapha on 22/5/2565 BE.
+//
+
+import EssentialFeed
+
+struct FeedViewModel {
+    let feed: [FeedImage]
+}
